37a22bd273e56d6e09fea3ee465c049451279929
tunogya <tunogya@qq.com>
2022-12-05 18:47:58 +0800

    add team

--- pages/about/team.tsx
+++ pages/about/team.tsx
@@ -1,4 +1,4 @@
-import {Stack, Text} from "@chakra-ui/react";
+import {HStack, Stack, Text} from "@chakra-ui/react";
 import Navigation from "../../components/Navigation";
 import Footer from "../../components/Footer";
 
@@ -6,7 +6,57 @@ const Page = () => {
   return (
     <Stack px={'45px'}>
       <Navigation/>
-      <Text>Home</Text>
+      <Stack textAlign={"center"} py={'167px'}>
+        <Text fontSize={'50px'} fontWeight={'bold'}>NEST DAO</Text>
+        <Stack h ='100px'>
+
+        </Stack>
+
+        <HStack justifyContent={"space-between"} fontWeight={'bold'} fontSize={'18px'}>
+          <Stack w={'250px'}>
+            <Text>NEST Research Academy</Text>
+            <Text>(NRA)</Text>
+          </Stack>
+          <Stack w={'250px'}>
+            <Text>NEST Influence Block</Text>
+            <Text>(NIB)</Text>
+          </Stack>
+          <Stack w={'250px'}>
+            <Text>NEST Developer Alliance</Text>
+            <Text>(NDA)</Text>
+          </Stack>
+          <Stack w={'250px'}>
+            <Text>NEST Community</Text>
+            <Text>(NC)</Text>
+          </Stack>
+        </HStack>
+
+        <Stack pt={'150px'} pb={'65px'}>
+          <Text fontSize={'33px'} fontWeight={'bold'}>Who is Behind NEST?</Text>
+          <Text fontSize={'15px'} fontWeight={'bold'}>The NEST protocol is coordinated by the NEST DAO and has the following organisation:</Text>
+        </Stack>
+
+        <Stack px={'160px'} spacing={'138px'}>
+          <Stack h={'440px'} bg={'red'} borderRadius={'20px'}>
+
+          </Stack>
+
+          <Stack h={'440px'} bg={'red'} borderRadius={'20px'}>
+
+          </Stack>
+
+          <Stack h={'440px'} bg={'red'} borderRadius={'20px'}>
+
+          </Stack>
+
+          <Stack h={'440px'} bg={'red'} borderRadius={'20px'}>
+
+          </Stack>
+
+        </Stack>
+
+
+      </Stack>
       <Footer/>
     </Stack>
   )
