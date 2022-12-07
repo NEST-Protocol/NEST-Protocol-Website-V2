0aebdf8feb03c7a82c4d39bfe4a543b89d8a8805
tunogya <tunogya@qq.com>
2022-12-07 11:27:28 +0800

    add blog image

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -8,27 +8,35 @@ const Page = () => {
   const [isMobile] = useMediaQuery("(max-width: 768px)");
 
   const pcPage = (
-    <Stack>
+    <Stack bgSize={'cover'} bgImage={"image/Blog/bg.jpg"} bgPosition={"center"}>
       <Navigation/>
       <Stack spacing={'44px'} px={'45px'}>
         <Wrap justify={'space-between'} spacing={'40px'}>
-          <WrapItem w={'30%'} bg={"red"} borderRadius={'20px'}>
-            <Stack h={'352px'}>
+          {
+            [
+              {image: '', title: '', desc: '', date: ''},
+              {image: '', title: '', desc: '', date: ''},
+              {image: '', title: '', desc: '', date: ''},
+            ].map((item, index) => (
+              <WrapItem key={index} w={'30%'} bg={"rgba(255, 255, 255, 0.7)"} borderRadius={'20px'}
+                        border={'1px solid #EEEEEE'}>
+                <Stack>
+                  <Stack h={'144px'}>
 
-            </Stack>
-          </WrapItem>
-          <WrapItem w={'30%'} bg={"red"} borderRadius={'20px'}>
-            <Stack h={'352px'}>
-
-            </Stack>
-          </WrapItem>
-          <WrapItem w={'30%'} bg={"red"} borderRadius={'20px'}>
-            <Stack h={'352px'}>
-
-            </Stack>
-          </WrapItem>
+                  </Stack>
+                  <Stack px={'44px'} py={'34px'}>
+                    <Text fontWeight={'bold'} fontSize={'18px'}>Coinbase Announces Planned Listing of Tokens, Adds
+                      $NEST</Text>
+                    <Text fontSize={'13px'} fontWeight={'600'}>On Monday, Coinbase, a leading American cryptocurrency
+                      exchange, announced that it would list six tokens </Text>
+                    <Text fontSize={'13px'} fontWeight={'500'}>July 25, 2022</Text>
+                  </Stack>
+                </Stack>
+              </WrapItem>
+            ))
+          }
         </Wrap>
-        <Stack py={'48px'} px={'44px'} borderRadius={'20px'}>
+        <Stack py={'48px'} px={'44px'} borderRadius={'20px'} bg={"rgba(255,255,255,0.8)"}>
           <HStack spacing={-1} justify={"center"} pb={'30px'}>
             <Button fontSize={'15px'} w={'150px'} h={'42px'} borderRadius={0} bg={'#EAAA00'} borderLeftRadius={'21px'}
                     border={'1px solid'} borderColor={'#EEEEEE'}>
@@ -48,54 +56,40 @@ const Page = () => {
             </Button>
           </HStack>
           <Divider/>
-          <Stack pt={'20px'} pb={'44px'}>
-            <Text fontWeight={'600'} fontSize={'25px'}>
-              NEST Roundtable 13:<br/>
-              What are the new NFT use cases, beyond music and art?
-            </Text>
-            <Text fontWeight={'500'} fontSize={'15px'} color={'#7D7D7D'}>
-              Nov 25, 2022
-            </Text>
-            <Text fontWeight={'600'} fontSize={'15px'}>
-              I think NFTs are bringing additional value to some of the old web 2 issues that we had. Some of the recent
-              ones for example have been with events and ticketing to go to a concert or to go to a sports event in the
-              form of NFT, and then as an NFT holder, you could use that ticket as a collectible. For example, the
-              people
-              that of NFT, and then as an NFT holder, you could use that ticket as a collectible. For example, the
-              people
-              that effectively, now with NFT, this is something that could be replicated except you wouldn’t have to
-              worry
-              about
-            </Text>
-          </Stack>
-          <Divider/>
-          <Stack pt={'20px'} pb={'44px'}>
-            <Text fontWeight={'600'} fontSize={'25px'}>
-              NEST Roundtable 13:<br/>
-              What are the new NFT use cases, beyond music and art?
-            </Text>
-            <Text fontWeight={'500'} fontSize={'15px'} color={'#7D7D7D'}>
-              Nov 25, 2022
-            </Text>
-            <Text fontWeight={'600'} fontSize={'15px'}>
-              I think NFTs are bringing additional value to some of the old web 2 issues that we had. Some of the recent
-              ones for example have been with events and ticketing to go to a concert or to go to a sports event in the
-              form of NFT, and then as an NFT holder, you could use that ticket as a collectible. For example, the
-              people
-              that of NFT, and then as an NFT holder, you could use that ticket as a collectible. For example, the
-              people
-              that effectively, now with NFT, this is something that could be replicated except you wouldn’t have to
-              worry
-              about
-            </Text>
-          </Stack>
-          <Divider/>
-
+          <Stack h={'44px'}></Stack>
+          {
+            [
+              {
+                title: 'What are the new NFT use cases, beyond music and art?', desc: `I think NFTs are bringing additional
+                  value to some of the old web 2 issues that we had. Some of the recent
+                  ones for example have been with events and ticketing to go to a concert or to go to a sports event in the
+                  form of NFT, and then as an NFT holder, you could use that ticket as a collectible. For example, the
+                  people that of NFT, and then as an NFT holder, you could use that ticket as a collectible. For example, the
+                  people that effectively, now with NFT, this is something that could be replicated except you wouldn’t have to
+                  worry about`, date: 'Nov 25, 2022'
+              },
+              {title: '', desc: '', date: ''},
+              {title: '', desc: '', date: ''},
+            ].map((item, index) => (
+              <Stack pb={'44px'} key={index}>
+                <Text fontWeight={'600'} fontSize={'25px'}>
+                  {item.title}
+                </Text>
+                <Text fontWeight={'500'} fontSize={'15px'} color={'#7D7D7D'}>
+                  {item.date}
+                </Text>
+                <Text fontWeight={'600'} fontSize={'15px'}>
+                  {item.desc}
+                </Text>
+                <Stack h={'44px'}></Stack>
+                <Divider/>
+              </Stack>
+            ))
+          }
           <Stack align={"center"} pt={'48px'}>
             <Button w={'140px'} variant={'outline'} h={'34px'} borderRadius={'17px'} border={'2px solid'}
                     color={'#EAAA00'} borderColor={'#EAAA00'}>More</Button>
           </Stack>
-
         </Stack>
       </Stack>
       <Footer/>
