1f438256f9845c188504412616c5748dff238297
tunogya <tunogya@qq.com>
2022-12-08 11:18:14 +0800

    update

--- components/NavigationMobile.tsx
+++ components/NavigationMobile.tsx
@@ -13,11 +13,13 @@ import {HamburgerIcon} from "@chakra-ui/icons";
 import gtag from "ga-gtag";
 import Link from "next/link";
 import {useState} from "react";
+import {useRouter} from "next/router";
 
 export default function NavigationMobile() {
   const {isOpen, onOpen, onClose} = useDisclosure()
   const [showDevelopers, setShowDevelopers] = useState(false)
   const [showAbout, setShowAbout] = useState(false)
+  const router = useRouter()
 
   return (
     <HStack w={'full'} px={'24px'} py={'20px'}>
@@ -53,36 +55,40 @@ export default function NavigationMobile() {
                   <Text fontSize={'16px'} fontWeight={'600'} opacity={showDevelopers || showAbout ? 0.6 : 1}>Cyber Ink</Text>
                 </Link>
                 <Link href={'/nest-fi/'}>
-                  <Text fontSize={'16px'} fontWeight={'600'} opacity={showDevelopers || showAbout ? 0.6 : 1}>NESTFi</Text>
+                  <Text fontSize={'16px'} fontWeight={'600'} color={router.pathname === '/nest-fi' ? '#EAAA00' : '#003232'} opacity={showDevelopers || showAbout ? 0.6 : 1}>NESTFi</Text>
                 </Link>
-                <Text fontSize={'16px'} fontWeight={'600'} opacity={showDevelopers || showAbout ? 0.6 : 1} onClick={() => setShowDevelopers(!showDevelopers)} >Developers</Text>
+                <Text fontSize={'16px'} fontWeight={'600'}
+                      opacity={showDevelopers || showAbout ? 0.6 : 1}
+                      onClick={() => setShowDevelopers(!showDevelopers)} >Developers</Text>
                 {
                   showDevelopers && (
                     <>
                       <Link href={'/developers/oracle/'}>
-                        <Text fontSize={'16px'} fontWeight={'600'}>Oracle</Text>
+                        <Text fontSize={'16px'} color={router.pathname === '/developers/oracle' ? '#EAAA00' : '#003232'} fontWeight={'600'}>Oracle</Text>
                       </Link>
                       <Link href={'/developers/pvm/'}>
-                        <Text fontSize={'16px'} fontWeight={'600'}>PVM</Text>
+                        <Text fontSize={'16px'} color={router.pathname === '/developers/pvm' ? '#EAAA00' : '#003232'} fontWeight={'600'}>PVM</Text>
                       </Link>
                     </>
                   )
                 }
                 <Link href={'/docs/'}>
-                  <Text fontSize={'16px'} opacity={showDevelopers || showAbout ? 0.6 : 1} fontWeight={'600'}>Doc</Text>
+                  <Text fontSize={'16px'} color={router.pathname === '/docs' ? '#EAAA00' : '#003232'}
+                        opacity={showDevelopers || showAbout ? 0.6 : 1}
+                        fontWeight={'600'}>Doc</Text>
                 </Link>
                 <Link href={'/blogs/'}>
-                  <Text fontSize={'16px'} opacity={showDevelopers || showAbout ? 0.6 : 1} fontWeight={'600'}>Blog</Text>
+                  <Text fontSize={'16px'} color={router.pathname === '/blogs' ? '#EAAA00' : '#003232'} opacity={showDevelopers || showAbout ? 0.6 : 1} fontWeight={'600'}>Blog</Text>
                 </Link>
                 <Text fontSize={'16px'} opacity={showDevelopers || showAbout ? 0.6 : 1} fontWeight={'600'} onClick={() => setShowAbout(!showAbout)}>About</Text>
                 {
                   showAbout && (
                     <>
-                      <Link href={'/about/team'}>
-                        <Text fontSize={'16px'} fontWeight={'600'}>Team</Text>
+                      <Link href={'/about/team/'}>
+                        <Text fontSize={'16px'} color={router.pathname === '/about/team' ? '#EAAA00' : '#003232'} fontWeight={'600'}>Team</Text>
                       </Link>
-                      <Link href={'/about/partnership'}>
-                        <Text fontSize={'16px'} fontWeight={'600'}>Partnership</Text>
+                      <Link href={'/about/partnership/'}>
+                        <Text fontSize={'16px'}  color={router.pathname === '/about/partnership' ? '#EAAA00' : '#003232'}  fontWeight={'600'}>Partnership</Text>
                       </Link>
                     </>
                   )
