407386135c06c77f2f60ab254672bef1e2c279c8
tunogya <tunogya@qq.com>
2023-08-04 10:50:23 +0800

    update

--- lib/roundtable.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const getAllRoundtable = async () => {
-  const req = await axios({
-    method: 'get',
-    url: 'https://cms.nestfi.net/cmsapi/roundtables?populate=invited_user_icons&sort=scheduled_start%3Adesc',
-  })
-  return req.data;
-}
--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -1,36 +1,47 @@
 import {Divider, Heading, HStack, Link, Stack, useMediaQuery} from "@chakra-ui/react";
 import Navigation from "../../components/Navigation";
-import {getAllBlogs, getBlog, getRecentBlogs} from "../../lib/blogs";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import Moment from "react-moment";
 import NavigationMobile from "../../components/NavigationMobile";
 import Head from "next/head";
+import {useRouter} from "next/router";
+import useSWR from "swr";
 
-export default function Page({content, date, recentBlogs, title, description, banner}: any) {
+export default function Page() {
   const [isMobile] = useMediaQuery("(max-width: 768px)");
+  const router = useRouter()
+  const slug = router.query.slug;
+  const {data: recentBlogs} = useSWR('https://cms.nestfi.net/cmsapi/blogs/?pagination[page]=1&pagination[pageSize]=5&fields[0]=slug&fields[1]=title&sort=date%3Adesc', (url) => fetch(url)
+    .then((res) => res.json())
+    .then((data) => data.data)
+  );
+  const {data: blogRes} = useSWR( slug ? `https://cms.nestfi.net/cmsapi/blogs/?filters[slug][$eq]=${slug}` : undefined, (url) => fetch(url)
+    .then((res) => res.json())
+    .then((data) => data.data[0])
+  );
 
   const SEO = (
     <Head>
-      <title>{title}</title>
-      { title && (
+      <title>{blogRes?.attributes?.title}</title>
+      { blogRes?.attributes?.title && (
         <>
-          <meta name="og:title" content={title}/>
-          <meta name="twitter:title" content={title}/>
+          <meta name="og:title" content={blogRes?.attributes?.title}/>
+          <meta name="twitter:title" content={blogRes?.attributes?.title}/>
         </>
       ) }
-      { description && (
+      { blogRes?.attributes?.description && (
         <>
-          <meta name="description" content={description}/>
-          <meta name="og:description" content={description}/>
-          <meta name="twitter:description" content={description}/>
+          <meta name="description" content={blogRes?.attributes?.description}/>
+          <meta name="og:description" content={blogRes?.attributes?.description}/>
+          <meta name="twitter:description" content={blogRes?.attributes?.description}/>
         </>
       ) }
-      { banner && (
+      { blogRes?.attributes?.banner?.data?.attributes?.url && (
         <>
-          <meta name="og:image" content={'https://cms.nestfi.net/' + banner}/>
-          <meta name="twitter:image" content={'https://cms.nestfi.net/' + banner}/>
+          <meta name="og:image" content={'https://cms.nestfi.net/' + blogRes?.attributes?.banner?.data?.attributes?.url}/>
+          <meta name="twitter:image" content={'https://cms.nestfi.net/' + blogRes?.attributes?.banner?.data?.attributes?.url}/>
         </>
       ) }
       <meta property="og:type" content="article" />
@@ -46,16 +57,16 @@ export default function Page({content, date, recentBlogs, title, description, ba
         <Stack minW={'400px'} w={'400px'} h={'full'} py={'40px'}>
           <Heading size={'md'}>Recent posts</Heading>
           {
-            recentBlogs.map((item: any) => (
+            recentBlogs?.map((item: any) => (
               <Link key={item.id} href={`/blogs/${item.attributes.slug}`}>{item.attributes.title}</Link>
             ))
           }
         </Stack>
         <Stack w={'container.xl'} h={'full'} p={'40px'}>
           {/* eslint-disable-next-line react/no-children-prop */}
-          <ReactMarkdown children={content} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} className={'markdown-body'}/>
+          <ReactMarkdown children={blogRes?.attributes?.content} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} className={'markdown-body'}/>
           <br/>
-          <Moment format="YYYY/MM/DD">{date}</Moment>
+          <Moment format="YYYY/MM/DD">{blogRes?.attributes?.date}</Moment>
         </Stack>
       </HStack>
     </Stack>
@@ -68,15 +79,15 @@ export default function Page({content, date, recentBlogs, title, description, ba
       <Divider/>
       <Stack w={'full'} h={'full'} p={'24px'}>
         {/* eslint-disable-next-line react/no-children-prop */}
-        <ReactMarkdown children={content} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} className={'markdown-body'}/>
+        <ReactMarkdown children={blogRes?.attributes?.content} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} className={'markdown-body'}/>
         <br/>
-        <Moment format="YYYY/MM/DD">{date}</Moment>
+        <Moment format="YYYY/MM/DD">{blogRes?.attributes?.date}</Moment>
       </Stack>
       <Divider/>
       <Stack w={'full'} h={'full'} p={'24px'}>
         <Heading size={'md'}>Recent posts</Heading>
         {
-          recentBlogs.map((item: any) => (
+          recentBlogs?.map((item: any) => (
             <Link key={item.id} href={`/blogs/${item.attributes.slug}`}>{item.attributes.title}</Link>
           ))
         }
@@ -93,28 +104,4 @@ export default function Page({content, date, recentBlogs, title, description, ba
   }
 }
 
-export const getStaticPaths = async () => {
-  const blogsRes = await getAllBlogs()
-  return {
-    paths: blogsRes.data.map((blog: any) => `/blogs/${blog.attributes.slug}`) || [],
-    fallback: false
-  }
-}
-
-export async function getStaticProps({params}: any) {
-  const blogRes = await getBlog(params.slug)
-  const recentBlogs = await getRecentBlogs()
-  return {
-    props: {
-      id: blogRes.id,
-      slug: blogRes.attributes.slug,
-      title: blogRes.attributes.title,
-      content: blogRes.attributes.content,
-      description: blogRes.attributes?.description || '',
-      banner: blogRes.attributes?.banner?.data?.attributes?.url || '',
-      date: blogRes.attributes.date,
-      recentBlogs: recentBlogs.data
-    }
-  }
-}
 
