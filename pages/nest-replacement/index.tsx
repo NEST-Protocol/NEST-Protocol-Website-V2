b44b0a01c3d3aec5af8ca3df1bf9781c489c2aa5
DoctorLi <doctorli2042@gmail.com>
2025-07-02 15:36:34 +0800

    Update index.tsx

--- pages/nest-replacement/index.tsx
+++ pages/nest-replacement/index.tsx
@@ -345,6 +345,10 @@ const Switch = () => {
             <HStack justifyContent={"center"}>
               <Text fontSize={'16px'} fontWeight={400} color={'rgba(3,3,8, 0.6)'}
                     lineHeight={'22px'}>A single address can only submit a request for a replacement token once.</Text>
+              <Text fontSize={'16px'} fontWeight={400} color={'rgba(3,3,8, 0.6)'}
+                    lineHeight={'22px'}>To request an exchange, please send an email to hello@nestprotocol.org.
+In your message, kindly include your wallet address and the amount you wish to exchange.
+We will review your request and follow up with the next steps upon approval.</Text>
             </HStack>
 {/*             <HStack spacing={'4px'} justify={'center'}>
               <Link href={'https://www.nestprotocol.org/blogs/Guide-of-NEST-2.0-token-replacement-on-Ethereum'}
@@ -713,6 +717,9 @@ const Switch = () => {
                     spacing={'4px'}>
               <Text color={'rgba(3,3,8, 0.6)'}>A single address can only submit a request for a replacement token
                 once.</Text>
+              <Text color={'rgba(3,3,8, 0.6)'}>To request an exchange, please send an email to hello@nestprotocol.org.
+In your message, kindly include your wallet address and the amount you wish to exchange.
+We will review your request and follow up with the next steps upon approval.</Text>
 {/*               <Link href={'https://www.nestprotocol.org/blogs/Guide-of-NEST-2.0-token-replacement-on-Ethereum'}
                     isExternal color={'#EAAA00'} ml={'4px'}>
                 Guide
